ef61c80f11ac88bfcc76c44b2de9edc72dcdb69c
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2019-05-17 15:20:06 -0400

    Updates

---
+++ precache-manifest.7e0bbfca02711e375656166d3cdce987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7b136390a200c35551b361c7b5fbf1",
+    "url": "/RickAndMorty-API/index.html"
+  },
+  {
+    "revision": "e3a4da4fade3f5244831",
+    "url": "/RickAndMorty-API/static/css/main.53f00067.chunk.css"
+  },
+  {
+    "revision": "80a031514111f031dba8",
+    "url": "/RickAndMorty-API/static/js/2.2d14f628.chunk.js"
+  },
+  {
+    "revision": "e3a4da4fade3f5244831",
+    "url": "/RickAndMorty-API/static/js/main.c900f54a.chunk.js"
+  },
+  {
+    "revision": "ac206a7ba8599774fbe5",
+    "url": "/RickAndMorty-API/static/js/runtime~main.de45ba3b.js"
+  },
+  {
+    "revision": "211b45aa6b0ab0619ead7de113ff1228",
+    "url": "/RickAndMorty-API/static/media/banner_img.211b45aa.jpg"
+  },
+  {
+    "revision": "b4a94b786bffe582cf5824950a8596f4",
+    "url": "/RickAndMorty-API/static/media/loading.b4a94b78.gif"
+  }
+]);
